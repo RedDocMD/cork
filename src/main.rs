25228c18e1dff8f2fdbb584cdf3a525064523f3f
Deep Majumder <deep.majumder2019@gmail.com>
2021-05-15 13:32:37 +0530

    Added in basic line-feed config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut rl = Editor::<()>::new();
+    loop {
+        match rl.readline("cork> ") {
+            Ok(line) => {
+                println!("{}", line);
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Exiting ... ");
+                break;
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Ctrl + C.\nPress Ctrl + D to exit");
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
 }
