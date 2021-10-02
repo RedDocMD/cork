125924441d4037c6965ba1d61fefd24270a46461
Deep Majumder <deep.majumder2019@gmail.com>
2021-10-02 12:43:31 +0530

    Config file being loaded and read

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,7 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
 use crate::format::FormatStyle;
+use anyhow::Result as AResult;
 use getset::Getters;
 use serde::Deserialize;
 
@@ -23,6 +26,40 @@ fn default_header() -> bool {
     true
 }
 
+fn config_locations() -> Vec<PathBuf> {
+    match home::home_dir() {
+        Some(home) => {
+            let mut at_home = home.clone();
+            at_home.push(".cork.yml");
+            let mut at_cork = home.clone();
+            at_cork.push(".cork");
+            at_cork.push("cork.yml");
+            let mut at_config = home.clone();
+            at_config.push(".config");
+            at_config.push("cork");
+            at_config.push("cork.yml");
+            vec![at_home, at_cork, at_config]
+        }
+        None => Vec::new(),
+    }
+}
+
+pub fn read_config() -> AResult<Config> {
+    let locations = config_locations();
+    let mut content = String::new();
+    for loc in &locations {
+        if loc.exists() && loc.is_file() {
+            let mut file = File::open(loc)?;
+            file.read_to_string(&mut content)?;
+        }
+    }
+    if content.is_empty() {
+        content = String::from("[]")
+    }
+    let config = serde_yaml::from_str(&content)?;
+    Ok(config)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -37,6 +74,31 @@ output_radix: Octal";
             prompt: String::from("$"),
             header: false,
             output_radix: FormatStyle::Octal,
+        };
+        assert_eq!(config, expected_config);
+    }
+
+    #[test]
+    fn test_config_deserialize_missing_values() {
+        let config_str = "prompt: $
+output_radix: Octal";
+        let config: Config = serde_yaml::from_str(config_str).unwrap();
+        let expected_config = Config {
+            prompt: String::from("$"),
+            header: default_header(),
+            output_radix: FormatStyle::Octal,
+        };
+        assert_eq!(config, expected_config);
+    }
+
+    #[test]
+    fn test_config_deserialize_empty() {
+        let config_str = "[]";
+        let config: Config = serde_yaml::from_str(config_str).unwrap();
+        let expected_config = Config {
+            prompt: default_prompt(),
+            header: default_header(),
+            output_radix: FormatStyle::default(),
         };
         assert_eq!(config, expected_config);
     }
